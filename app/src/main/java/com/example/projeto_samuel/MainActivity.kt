7a14784ee782e96166fb5fc0e4bd7b6581eb37c0
Samuel <samuelsantanadasilva8@gmail.com>
2023-05-23 17:01:28 -0300

    Atualizando a mainActivity do meu aplicativo

--- app/src/main/java/com/example/projeto_samuel/MainActivity.kt
+++ app/src/main/java/com/example/projeto_samuel/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,13 +12,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fun abrirOutraPagina() {
-            val intent = Intent(this, investimentos_curto_prazo::class.java)
-            startActivity(intent)
+        val curtoPrazoTextView = findViewById<TextView>(R.id.curtoPrazo)
+
+        curtoPrazoTextView.setOnClickListener {
+            paginaCurtoPrazo()
         }
 
+        val medioPrazoTextView = findViewById<TextView>(R.id.medioPrazo)
 
+        medioPrazoTextView.setOnClickListener {
+            paginaMedioPrazo()
+        }
 
+        val longoPrazoTextView = findViewById<TextView>(R.id.longoPrazo)
+
+        longoPrazoTextView.setOnClickListener {
+            paginaLongPrazo()
+        }
+
+    }
+
+    fun paginaCurtoPrazo() {
+        val curtoPrazo = Intent(this, investimentos_curto_prazo::class.java)
+        startActivity(curtoPrazo)
+    }
+
+    fun paginaMedioPrazo() {
+        val medioPrazo = Intent(this, investimento_medio_prazo::class.java)
+        startActivity(medioPrazo)
+    }
+
+    fun paginaLongPrazo() {
+        val longoPrazo = Intent(this, investimento_longo_prazo::class.java)
+        startActivity(longoPrazo)
     }
 }
 
