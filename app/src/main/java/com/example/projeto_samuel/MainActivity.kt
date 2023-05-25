983269d401bfefa4da597e7f8cc4c05b9f8f8863
Samuel <samuelsantanadasilva8@gmail.com>
2023-05-25 16:39:45 -0300

    ultimos ajustes do projeto

--- app/src/main/java/com/example/projeto_samuel/MainActivity.kt
+++ app/src/main/java/com/example/projeto_samuel/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.projeto_samuel
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+//import android.widget.Button
 import android.widget.TextView
+import android.view.Menu
+import android.view.MenuItem
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+      /*  val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.menu, menu)*/
+
+
 
         val curtoPrazoTextView = findViewById<TextView>(R.id.curtoPrazo)
 
@@ -32,19 +39,63 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun paginaCurtoPrazo() {
+
+  /*  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        //val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }*/
+
+     fun paginaCurtoPrazo() {
         val curtoPrazo = Intent(this, investimentos_curto_prazo::class.java)
         startActivity(curtoPrazo)
     }
 
-    fun paginaMedioPrazo() {
+     fun paginaMedioPrazo() {
         val medioPrazo = Intent(this, investimento_medio_prazo::class.java)
         startActivity(medioPrazo)
     }
 
-    fun paginaLongPrazo() {
+     fun paginaLongPrazo() {
         val longoPrazo = Intent(this, investimento_longo_prazo::class.java)
         startActivity(longoPrazo)
+    }
+
+    //Lozalizou o menu que é no arquivo menu.xml
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    //Direcionamentos para dos link quando forem clicados para as outras paginas
+    override  fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.curtoPrazo -> {
+                val curtoPrazo = Intent(this, investimentos_curto_prazo::class.java)
+                startActivity(curtoPrazo)
+                true
+            }
+            R.id.medioPrazo -> {
+                val medioPrazo = Intent(this, investimento_medio_prazo::class.java)
+                startActivity(medioPrazo)
+                true
+            }
+            R.id.longoPrazo -> {
+                val longoPrazo = Intent(this, investimento_longo_prazo::class.java)
+                startActivity(longoPrazo)
+                true
+            }
+            R.id.activityMain -> {
+                val paginaPrincipal = Intent(this, MainActivity::class.java)
+                startActivity(paginaPrincipal)
+                true
+            }
+            else -> {
+                false
+                //super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
 
--- app/src/main/java/com/example/projeto_samuel/menu.kt
+++ app/src/main/java/com/example/projeto_samuel/menu.kt
@@ -1,7 +1,7 @@
 package com.example.projeto_samuel
 
 import android.content.Intent
-import android.os.Bundle
+/*import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
@@ -15,44 +15,15 @@ class  menu : AppCompatActivity() {
 
     }
 
-    //Lozalizou o menu que é no arquivo menu.xml
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-            menuInflater.inflate(R.menu.menu, menu)
-            return super.onCreateOptionsMenu(menu)
-        }
 
-     /*  override fun onPrepareOptionsMenu(menu: Menu): Boolean {
-
-            val menuChecked = menu.findItem(R.id.curtoPrazo)
-            if(menuChecked.isChecked){
-                menuItem = menu.add("AD")
-            }
-
-            return super.onPrepareOptionsMenu(menu)
-        }*/
-
-    //Direcionamentos para dos link quando forem clicados para as outras paginas
-       override  fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId){
-            R.id.curtoPrazo -> {
-                val curtoPrazo = Intent(this, investimentos_curto_prazo::class.java)
-                startActivity(curtoPrazo)
-                true
-            }
-            R.id.medioPrazo -> {
-                val medioPrazo = Intent(this, investimento_medio_prazo::class.java)
-                startActivity(medioPrazo)
-                true
-            }
-            R.id.longoPrazo -> {
-                val longoPrazo = Intent(this, investimento_longo_prazo::class.java)
-                startActivity(longoPrazo)
-                true
-            }
-            else -> {
-                super.onOptionsItemSelected(item)
-            }
-        }
-    }
 
 }
+ override fun onPrepareOptionsMenu(menu: Menu): Boolean {
+
+         val menuChecked = menu.findItem(R.id.curtoPrazo)
+         if(menuChecked.isChecked){
+             menuItem = menu.add("AD")
+         }
+
+         return super.onPrepareOptionsMenu(menu)
+     }*/
